d228e0dd92b03312dd93224e9baf82e74df068af
Foswa <fcahnevrard@gmail.com>
2022-12-15 01:11:33 +0200

    Added some js logic for displaying unit text and image when the respective icon is clicked.

--- main.js
+++ main.js
@@ -1,17 +1,50 @@
+// const maaButton = document.getElementById('MAA-Button');
+// const childDiv2 = document.getElementById('MAA-text');
+// const childDiv3= document.getElementById('singleMAAUnit');
+
+// function divHider(){
+//     if(childDiv2.style.display != 'none'){
+//         childDiv2.style.display ='none';
+//     }
+//     else {
+//         childDiv2.style.display = 'block'
+//     }
+// }
+
+// function divHider2(){
+//     if(childDiv3.style.display != 'none'){
+//         childDiv3.style.display ='none';
+//     }
+//     else {
+//         childDiv3.style.display = 'block'
+//     }
+// }
+// maaButton.addEventListener('click', divHider);
+// maaButton.addEventListener('click', divHider2);
+
+
+
+
+
 const maaButton = document.getElementById('MAA-Button');
-const maaImage = document.getElementById('singleMAAUnit')
+const maaText = document.getElementById('MAA-text');
+const maaImage = document.getElementById('singleMAAUnit');
 
-let isImageVisible = false;
+const landButton = document.getElementById('land-button');
+const landText = document.getElementById('landknecht-text');
+const landImage = document.getElementById('landknetcht-unit');
 
-function toggleImage() {
-    isImageVisible = !isImageVisible;
-    if (isImageVisible){
-        maaImage.style.display = 'block'
-    } else {
-        maaImage.style.display = 'none'
-    }
+function divHider(dividerName) {
+  if (dividerName.style.display != 'none') {
+    dividerName.style.display = 'none';
+  } else {
+    dividerName.style.display = 'block';
+  }
 }
 
+landButton.addEventListener('click', function(){divHider(landText)});
+landButton.addEventListener('click', function(){divHider(landImage)});
 
+maaButton.addEventListener('click', function() { divHider(maaText); });
+maaButton.addEventListener('click', function() { divHider(maaImage); });
 
-maaButton.addEventListener('click',toggleImage);
